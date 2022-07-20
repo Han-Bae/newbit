8872a05b18f983027253b551cadd27e4bbd3c7f8
jan <samjan1127@gmail.com>
2022-07-21 00:00:11 +0900

    SQL.xml 에러 수정

--- src/main/java/com/newbit/www/dao/StoreDao.java
+++ src/main/java/com/newbit/www/dao/StoreDao.java
@@ -1,5 +1,10 @@
 package com.newbit.www.dao;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class StoreDao {
 
+	@Autowired
+	SqlSessionTemplate sqlSession;
 }
