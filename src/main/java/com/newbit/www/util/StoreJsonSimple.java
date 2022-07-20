75ad53a5522f01f81873576d36dc13e08a8a2ccf
jan <samjan1127@gmail.com>
2022-07-20 15:54:43 +0900

    Json simple 참고용 Class Commit

--- src/main/java/com/newbit/www/util/StoreJsonSimple.java
+++ src/main/java/com/newbit/www/util/StoreJsonSimple.java
@@ -3,7 +3,9 @@ package com.newbit.www.util;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.ArrayList;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
@@ -15,17 +17,41 @@ public class StoreJsonSimple {
 		StoreVO sVO = new StoreVO();
 		
 		try {
+			// json url
 			URL url = new URL("https://store.steampowered.com/api/appdetails?appids=" + appId + "&l=korean");
 			BufferedReader bf = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"));
 			String result = bf.readLine();
 			
 			JSONParser jsonParser = new JSONParser();
+			// JSONObject; 예시 => data: {}
+			// JSONArray;  예시 => developers: []
 			JSONObject jsonObject = (JSONObject) jsonParser.parse(result);
 			JSONObject appJson = (JSONObject) jsonObject.get(appId);
 			JSONObject appData = (JSONObject) appJson.get("data");
 			
-			String appType = (String) appData.get("type");
-			System.out.println(appType);
+			sVO.setType((String) appData.get("type"));
+			sVO.setTitle((String) appData.get("name"));
+			sVO.setDetailedDescription((String) appData.get("detailed_description"));
+			if(((String) appData.get("type")).equals("dlc")) {
+				sVO.setFullgameId((String) ((JSONObject) appData.get("fullgame")).get("appid"));
+			} else if(((String) appData.get("type")).equals("game")) {
+				sVO.setShortDescription((String) appData.get("short_description"));
+			}
+			sVO.setImg((String) appData.get("header_image"));
+			ArrayList<String> developers = new ArrayList<String>();
+			JSONArray devArr = (JSONArray) appData.get("developers");
+			for(int i = 0 ; i < devArr.size() ; i++) {
+				developers.add((String) devArr.get(i));
+			}
+			sVO.setDevelopers(developers);
+			ArrayList<String> publishers = new ArrayList<String>();
+			JSONArray publArr = (JSONArray) appData.get("publishers");
+			for(int i = 0 ; i < publArr.size() ; i++) {
+				publishers.add((String) publArr.get(i));
+			}
+			sVO.setPublishers(publishers);
+			JSONObject priceOverview = (JSONObject) appData.get("price_overview");
+			sVO.setDiscount((String) priceOverview.get("discount_percent"));
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
